d10e5eb07ce1547e6040205c36c6c6aaab382683
Umi Sato <UmiSato42@gmail.com>
2023-11-15 15:29:36 -0500

    Add files

---
+++ script/script.js
@@ -0,0 +1,22 @@
+
+function showDisplay() {
+    var gridElements = document.getElementsByClassName("grid-items2");
+    var button = document.getElementById("button");
+    for (var x = 0; x <= gridElements.length; x++){
+        if (gridElements[x].style.display === "block"){
+            gridElements[x].style.display = "none";
+            button.innerHTML = "See more";
+        } else {
+            gridElements[x].style.display = "block";
+            button.innerHTML = "See less";
+        }
+    }
+}
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+  }
